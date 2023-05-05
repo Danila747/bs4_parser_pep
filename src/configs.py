eb1b99dd573da04759663dc0c9ef9bfcd3611fbc
Daniel <105740743+Danila747@users.noreply.github.com>
2023-05-05 17:17:31 +0200

    Update configs.py

--- src/configs.py
+++ src/configs.py
@@ -1,15 +1,19 @@
 import argparse
 import logging
+import os
 
 from logging.handlers import RotatingFileHandler
+
 from constants import (
     BASE_DIR,
     DT_FORMAT,
     LOG_FORMAT
 )
 
-
 def configure_argument_parser(available_modes):
+    """
+    Создает аргумент-парсер.
+    """
     parser = argparse.ArgumentParser(description='Парсер документации Python')
     parser.add_argument(
         'mode',
@@ -31,16 +35,34 @@ def configure_argument_parser(available_modes):
     return parser
 
 
+def setup_logging(log_level=logging.INFO, log_format='%(asctime)s %(levelname)s %(name)s %(message)s',
+                  log_filename='parser.log'):
+    """
+    Настраивает логирование.
+    """
+    log_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "logs")
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+
+
 def configure_logging():
     log_dir = BASE_DIR / "logs"
     log_dir.mkdir(exist_ok=True)
+
+
     log_file = log_dir / "parser.log"
+    max_log_size = 10 * 1024 * 1024
     rotating_handler = RotatingFileHandler(
-        log_file, maxBytes=pow(10, 6), backupCount=5
+        log_file, maxBytes=max_log_size, backupCount=5
     )
+
+    console_handler = logging.StreamHandler()
+    handlers = [rotating_handler, console_handler]
+
     logging.basicConfig(
         level=logging.INFO,
         format=LOG_FORMAT,
         datefmt=DT_FORMAT,
-        handlers=(rotating_handler, logging.StreamHandler())
+        handlers=handlers
     )
