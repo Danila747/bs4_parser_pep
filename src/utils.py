90b385d5ecbe081452c956331521a30fb1272ad9
Daniel <105740743+Danila747@users.noreply.github.com>
2023-05-07 15:03:24 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -19,29 +19,10 @@ def get_response(session: Session, url: str):
         return None
 
 
-def find_tag(soup: Bs, tag: Optional[str] = None,
-             attrs: Optional[dict] = None,
-             text: Optional[str] = None) -> Optional[Bs]:
-    try:
-        if text:
-            searched_tag = soup.find(text=text)
-        else:
-            searched_tag = soup.find(tag, attrs=(attrs or {}))
-        if searched_tag is None:
-            raise Exception
-    except Exception:
-        if text:
-            error_msg = "Не найден тег unexpected None"
-        else:
-            error_msg = "Не найден тег unexpected None"
-        logging.error(error_msg, exc_info=True)
+def find_tag(soup, tag, attrs=None):
+    searched_tag = soup.find(tag, attrs=(attrs or {}))
+    if searched_tag is None:
+        error_msg = f'Не найден тег {tag} {attrs}'
+        logging.error(error_msg, stack_info=True)
         raise ParserFindTagException(error_msg)
-    else:
-        return searched_tag
-
-
-def check_key(key: str) -> bool:
-    if key in EXPECTED_STATUS:
-        return True
-    else:
-        raise KeyError("Status key not found in the dictionary")
+    return searched_tag
