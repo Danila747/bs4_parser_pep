14ecc9bbe7658f6ccc4705eff2c1c67ead9ddb24
Daniel <105740743+Danila747@users.noreply.github.com>
2023-05-07 14:56:11 +0200

    Update configs.py

--- src/configs.py
+++ src/configs.py
@@ -1,63 +1,35 @@
 import argparse
 import logging
-import os
 from logging.handlers import RotatingFileHandler
+import sys
 
-from constants import BASE_DIR, DT_FORMAT, LOG_FORMAT
+from constants import DT_FORMAT, LOG_FORMAT, LOG_DIR
 
 
 def configure_argument_parser(available_modes):
-    """
-    Создает аргумент-парсер.
-    """
-    parser = argparse.ArgumentParser(description='Парсер документации Python')
+    parser = argparse.ArgumentParser()
     parser.add_argument(
-        'mode',
-        choices=available_modes,
-        help='Режимы работы парсера'
+        '--clear-cache',
+        action='store_true',
+        help='Очистить кэш'
     )
     parser.add_argument(
-        "-c",
-        "--clear-cache",
-        action="store_true",
-        help="Очистка кеша"
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        choices=("pretty", "file"),
-        help="Дополнительные способы вывода данных"
+        '--output',
+        choices=('pretty'),
+        help='Способ вывода'
     )
     return parser
 
 
-def setup_logging(log_level=logging.INFO,
-                  log_format='%(asctime)s %(levelname)s %(name)s %(message)s',
-                  log_filename='parser.log'):
-    """
-    Настраивает логирование.
-    """
-    log_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), "logs")
-    if not os.path.exists(log_dir):
-        os.makedirs(log_dir)
-
-
 def configure_logging():
-    log_dir = BASE_DIR / "logs"
-    log_dir.mkdir(exist_ok=True)
-
-    log_file = log_dir / "parser.log"
-    max_log_size = 10 * 1024 * 1024
+    LOG_DIR.mkdir(exist_ok=True)
+    log_file = LOG_DIR / 'parser.log'
     rotating_handler = RotatingFileHandler(
-        log_file, maxBytes=max_log_size, backupCount=5
+        log_file, maxBytes=10 ** 6, backupCount=5
     )
-
-    console_handler = logging.StreamHandler()
-    handlers = [rotating_handler, console_handler]
-
     logging.basicConfig(
-        level=logging.INFO,
-        format=LOG_FORMAT,
         datefmt=DT_FORMAT,
-        handlers=handlers
+        format=LOG_FORMAT,
+        level=logging.INFO,
+        handlers=(rotating_handler, logging.StreamHandler(sys.stdout))
     )
