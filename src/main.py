21ffed6804118ddc17bfff11640e9a8ba27a40cd
Daniel <105740743+Danila747@users.noreply.github.com>
2023-05-12 08:46:19 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -42,23 +42,27 @@ def latest_versions(session):
     soup = BeautifulSoup(response.text, 'lxml')
     sidebar = find_tag(soup, 'div', attrs={'class': 'sphinxsidebarwrapper'})
     ul_tags = sidebar.find_all('ul')
+    
     for ul in ul_tags:
         if 'All versions' in ul.text:
             a_tags = ul.find_all('a')
             break
     else:
         raise Exception('Ничего не нашлось')
+    
+    if not a_tags:
+        raise Exception('Ссылки не найдены')
+
     results = [('Ссылка на документацию', 'Версия', 'Статус')]
     pattern = r'Python (?P<version>\d\.\d+) \((?P<status>.*)\)'
     for a_tag in a_tags:
         link = a_tag['href']
         text_match = re.search(pattern, a_tag.text)
-        if text_match is not None:
-            version, status = text_match.groups()
-        else:
-            version, status = a_tag.text, ''
+        version, status = text_match.groups() if text_match else (a_tag.text, '')
         results.append((link, version, status))
+    
     return results
+
 
 
 def download(session):
@@ -97,26 +101,30 @@ def pep(session):
         main_card_tag = find_tag(soup, 'section', {'id': 'pep-content'})
         main_card_dl_tag = find_tag(main_card_tag, 'dl',
                                     {'class': 'rfc2822 field-list simple'})
-        for tag in main_card_dl_tag:
-            if tag.name == 'dt' and tag.text == 'Status:':
-                card_status = tag.next_sibling.next_sibling.string
-                count_status_in_card[card_status] = count_status_in_card.get(
-                    card_status, 0) + 1
-                if len(peps_row[i].td.text) != 1:
-                    table_status = peps_row[i].td.text[1:]
-                    if card_status[0] != table_status:
-                        logging.info(
-                            '\n'
-                            'Несовпадающие статусы:\n'
-                            f'{pep_link}\n'
-                            f'Статус в карточке: {card_status}\n'
-                            f'Ожидаемые статусы: '
-                            f'{EXPECTED_STATUS[table_status]}\n'
-                                )
+    for tag in main_card_dl_tag:
+        if tag.name != 'dt' or tag.text != 'Status:':
+            continue
+
+        card_status = tag.next_sibling.next_sibling.string
+        count_status_in_card[card_status] = count_status_in_card.get(card_status, 0) + 1
+
+        if len(peps_row[i].td.text) != 1:
+            table_status = peps_row[i].td.text[1:]
+            if card_status[0] != table_status:
+                logging.info(
+                    '\n'
+                    'Несовпадающие статусы:\n'
+                    f'{pep_link}\n'
+                    f'Статус в карточке: {card_status}\n'
+                    f'Ожидаемые статусы: {EXPECTED_STATUS[table_status]}\n'
+                )
+
     for key in count_status_in_card:
         result.append((key, str(count_status_in_card[key])))
-    result.append(('Total', len(peps_row)-1))
+
+    result.append(('Total', len(peps_row) - 1))
     return result
+
 
 
 MODE_TO_FUNCTION = {
